0ee99b051b369b00ca46881a6aff674496cb01e8
ewtlyn <alaskalove666@gmail.com>
2025-03-20 10:59:01 +0300

    але

---
+++ Desktop/sincos/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите начальную точку a:");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите конечную точку b:");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Введите шаг h:");
+        double h = Convert.ToDouble(Console.ReadLine());
+
+        //вывод заголовка таблицы
+        Console.WriteLine("x\t\ty(x)");
+
+        //вычисление и вывод значений функции
+        for (double x = a; x <= b; x += h)
+        {
+            double y = Math.Cos(x * x) + Math.Sin(x);
+            Console.WriteLine($"{x}\t\t{y}");
+        }
+    }
+}
